5c8fb33c09c745937114245b37f4727e6d4fd62e
AdithyaKU <kuadithya@gmail.com>
2020-10-01 14:27:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,28 +2,26 @@ const request = require('request');
 const express = require('express');
 const assert = require('assert');
 const data = JSON.stringify({
-     title: 'foo', body: 'bar', userId: 1
+  title: 'foo',
+  body: 'bar',
+  userId: 1
 });
-//var myJSONObject = { title: 'foo', body: 'bar', userId: 1 };
 
 const options = {
-    url: 'https://jsonplaceholder.typicode.com/posts' ,
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8',
-				'Content-Length': data.length
-    //   'User-Agent': 'my-reddit-client'
-    }
-
-
+  url: 'https://jsonplaceholder.typicode.com/posts',
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Accept-Charset': 'utf-8',
+    'Content-Length': data.length
+  }
 };
 
 request(options, function(err, res, body) {
-    let json = JSON.parse(body);
-    console.log(json);
-    console.log(json.id);
-    assert.strictEqual(res.statusCode, 201);
-    assert.strictEqual(json.id, 101);
+  let json = JSON.parse(body);
+  console.log(json);
+  console.log(json.id);
+  assert.strictEqual(res.statusCode, 201);
+  assert.strictEqual(json.id, 101);
 
-}) ;
+});
