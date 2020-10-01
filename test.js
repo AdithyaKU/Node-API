8e4038810f8a0c03cbee234f79f469d98f5dcdda
AdithyaKU <kuadithya@gmail.com>
2020-10-01 14:30:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,36 +1,32 @@
-
-
 const fs = require("fs");
 const xml2js = require('xml2js');
 
 // read XML file
 fs.readFile("D:\\Node js\\PostJson\\database.xml", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  // convert XML data to JSON object
+  xml2js.parseString(data, (err, result) => {
     if (err) {
-        throw err;
+      throw err;
     }
 
-    // convert XML data to JSON object
-    xml2js.parseString(data, (err, result) => {
-        if (err) {
-            throw err;
-        }
+    // replace `Wipro` with `Wipro Ltd`
+    result.database.datas[0].name = 'Wipro Ltd';
 
-        // replace `Wipro` with `Wipro Ltd`
-        result.database.datas[0].name = 'Wipro Ltd';
+// convert JSON object to XML
+    const builder = new xml2js.Builder();
+    const xml = builder.buildObject(result);
 
+    // write updated XML string to a file
+    fs.writeFile('new-databases.xml', xml, (err) => {
+      if (err) {
+        throw err;
+      }
 
-
-        // convert JSON object to XML
-          const builder = new xml2js.Builder();
-          const xml = builder.buildObject(result);
-
-          // write updated XML string to a file
-          fs.writeFile('new-databases.xml', xml, (err) => {
-              if (err) {
-                  throw err;
-              }
-
-              console.log(`Updated XML is written to a new file.`);
-          });
-        });
+      console.log(`Updated XML is written to a new file.`);
+    });
+  });
 });
