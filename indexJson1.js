582818826a571f5320c1a939cf379fd657f4cea6
AdithyaKU <kuadithya@gmail.com>
2020-10-01 14:24:30 +0530

    Update indexJson1.js

--- indexJson1.js
+++ indexJson1.js
@@ -3,20 +3,20 @@ const fs = require("fs");
 // Read company.json file
 fs.readFile("company.json", function(err, data) {
 
-    // Check for errors
-    if (err) throw err;
+  // Check for errors
+  if (err) throw err;
 
-    // Converting to JSON
-    const company = JSON.parse(data);
-    company[0]['company name'] = 'Wipro Ltd';
+  // Converting to JSON
+  const company = JSON.parse(data);
+  company[0]['company name'] = 'Wipro Ltd';
 
-    console.log(company);
-    fs.writeFile('new-company.json', company, (err) => {
-        if (err) {
-            throw err;
-        }
+  console.log(company);
+  fs.writeFile('new-company.json', company, (err) => {
+    if (err) {
+      throw err;
+    }
 
-        console.log(`Updated json is written to a new file.`);
-    });
+    console.log(`Updated json is written to a new file.`);
+  });
 
 });
